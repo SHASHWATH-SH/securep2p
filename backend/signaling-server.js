3f04194ffa12c8d660e2efbe57a6f0e6a00088d3
ShashwathSH <shashwathsh.is22@rvce.edu.in>
2025-07-16 12:05:05 +0530

    Initial commit for securep2p

--- backend/signaling-server.js
+++ backend/signaling-server.js
@@ -111,6 +111,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
